cc2ce01198d06109c37ba07bab8f80215883c133
cuongduong2710 <cuongduongngocquoc@gmail.com>
2024-10-02 22:25:57 +0700

    create a server by using http, listen on port 3000 and response client

---
+++ finly/index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const { url } = req
+    console.log(req.url)
+    if (url === '/') {
+        res.end('Hello From Node.js')
+    } else if (url === '/contact') {
+        res.end('The Contact page')
+    } else if (url === '/about') {
+        res.end('The About page')
+    } else {
+        res.writeHead(404)
+        res.end('Not Found')
+    }
+    // res.end('Hello From Node.js')
+})
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
+// req.url
+// http://localhost:3000/about -> /about 
+// http://localhost:3000/contact -> /contact
